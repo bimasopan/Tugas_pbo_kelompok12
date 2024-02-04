a44acc200d10b571f4e65e4d5c8b3b092e67a38e
Confrontg1 <99866452+Confrontg1@users.noreply.github.com>
2024-02-05 00:14:24 +0700

    Update FormulirData.java

--- FormulirData.java
+++ FormulirData.java
@@ -1,1 +1,44 @@
+import java.util.*;
 
+public class FormulirData {
+    private String namaLengkap;
+    private String alamat;
+    private String telepon;
+
+    public FormulirData() {
+        inputData();
+    }
+
+    public void inputData() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nama Lengkap: ");
+        this.namaLengkap = scanner.nextLine();
+
+        System.out.print("Alamat: ");
+        this.alamat = scanner.nextLine();
+
+        System.out.print("Nomor Telepon: ");
+        this.telepon = scanner.nextLine();
+    }
+
+    public List<String> getDataList() {
+        List<String> data = new ArrayList<>();
+        data.add(namaLengkap);
+        data.add(alamat);
+        data.add(telepon);
+        return data;
+    }
+
+    public String getNamaLengkap() {
+        return namaLengkap;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+}
